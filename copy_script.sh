1051dc6eabe839b1296e3d8d79f4b16d0f4dc076
jimmyjhickey <jimmyjhickey@gmail.com>
2019-09-07 19:14:50 -0400

    Started script to copy config files

---
+++ copy_script.sh
@@ -0,0 +1,10 @@
+###
+# Jimmy Hickey
+# 2019-09-07
+# A script to copy config files into this repo
+###
+
+BASH_ALIAS_LOCATION="/Users/jimmy/.bash_aliases"
+BASH_ALIAS_FILE="bash_aliases"
+
+cp ${BASH_ALIAS_LOCATION} ${BASH_ALIAS_FILE}
