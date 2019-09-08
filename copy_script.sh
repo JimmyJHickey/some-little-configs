85e9db49f60ad438e227d1bc1231719fad1cafc6
jimmyjhickey <jimmyjhickey@gmail.com>
2019-09-08 09:56:20 -0400

    Added crontab file

--- copy_script.sh
+++ copy_script.sh
@@ -9,15 +9,22 @@ git pull
 
 # Start copying config files
 
+# Bash Aliases
 BASH_ALIAS_LOCATION="/Users/jimmy/.bash_aliases"
 BASH_ALIAS_FILE="bash_aliases"
 
 cp ${BASH_ALIAS_LOCATION} ${BASH_ALIAS_FILE}
 
+# Vim RC
 VIM_RC_LOCATION="/Users/jimmy/.vimrc"
 VIM_RC_FILE="vimrc"
 
 cp ${VIM_RC_LOCATION} ${VIM_RC_FILE}
+
+# Cron jobs
+CRONTAB_FILE="crontab"
+crontab -e > ${CRONTAB_FILE}
+
 
 # Commit new changes
 DATE="$(date '+%Y-%m-%d %H:%M:%S')"
